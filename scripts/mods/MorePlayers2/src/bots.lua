2686d9b12c3a9720bf4a39c11b6c37314b55775a
Dan Reeves <hey@danreev.es>
2021-06-04 04:49:29 +0100

    4.4 patch

--- scripts/mods/MorePlayers2/src/bots.lua
+++ scripts/mods/MorePlayers2/src/bots.lua
@@ -4,9 +4,9 @@ local mod = get_mod("MorePlayers2")
 script_data.cap_num_bots = mod:get("num_bots")
 
 mod:hook(GameModeAdventure, "_get_first_available_bot_profile", function ()
-  return 3, 2
+  return 4, 4
 end)
 
 mod:hook(GameModeDeus, "_get_first_available_bot_profile", function ()
-  return 3, 2
+  return 4, 4
 end)
