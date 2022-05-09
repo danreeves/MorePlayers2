0b21a32d80986fdb02ef8febd90e27d0a237c535
Dan Reeves <hey@danreev.es>
2022-05-09 23:44:17 +0100

    fix

--- scripts/mods/MorePlayers2/MorePlayers2.lua
+++ scripts/mods/MorePlayers2/MorePlayers2.lua
@@ -2,13 +2,13 @@
 local mod = get_mod("MorePlayers2")
 
 function mod.on_all_mods_loaded()
-  mod.mmo_names = get_mod("MMONames2")
+	mod.mmo_names = get_mod("MMONames2")
 end
 
-mod.VERSION = "0.32"
+mod.VERSION = "0.33"
 mod.MOD_NAME = "[BETA] BTMP"
 mod.MAX_PLAYERS = mod:get("max_players") or 32
-mod.ID = "2113204803"-- Steam Workshop ID
+mod.ID = "2113204803" -- Steam Workshop ID
 
 -- Core
 mod:dofile("scripts/mods/MorePlayers2/src/core")
@@ -37,42 +37,42 @@ mod:dofile("scripts/mods/MorePlayers2/src/ui/server_browser")
 mod:dofile("scripts/mods/MorePlayers2/src/qol/unlock_cata")
 
 function mod.update_lobby_data()
-  if Managers.player.is_server then
-    Managers.lobby:setup_network_options()
-    local lobby_data = Managers.matchmaking.lobby:get_stored_lobby_data()
-    local old_server_name = LobbyAux.get_unique_server_name()
-    local unique_server_name = "[BTMP] " .. old_server_name
-    lobby_data.unique_server_name = unique_server_name
-    lobby_data.btmp_max_players = tostring(mod:get("max_players"))
-    Managers.matchmaking.lobby:set_lobby_data(lobby_data)
-  end
+	if Managers.player.is_server then
+		Managers.lobby:setup_network_options()
+		local lobby_data = Managers.matchmaking.lobby:get_stored_lobby_data()
+		local old_server_name = LobbyAux.get_unique_server_name()
+		local unique_server_name = "[BTMP] " .. old_server_name
+		lobby_data.unique_server_name = unique_server_name
+		lobby_data.btmp_max_players = tostring(mod:get("max_players"))
+		Managers.matchmaking.lobby:set_lobby_data(lobby_data)
+	end
 end
 
 function mod.on_setting_changed()
-  mod.MAX_PLAYERS = mod:get("max_players")
-  script_data.cap_num_bots = math.min(mod:get("num_bots"), mod.MAX_PLAYERS)
-  mod.update_lobby_data()
+	mod.MAX_PLAYERS = mod:get("max_players")
+	script_data.cap_num_bots = math.min(mod:get("num_bots"), mod.MAX_PLAYERS)
+	mod.update_lobby_data()
 end
 
-ModManager.unload_mod = function (self, index)
-  local m = self._mods[index]
+ModManager.unload_mod = function(self, index)
+	local m = self._mods[index]
 
-  if m then
-    self:print("info", "Unloading %q.", m.name)
-    self:_run_callback(m, "on_unload")
+	if m then
+		self:print("info", "Unloading %q.", m.name)
+		self:_run_callback(m, "on_unload")
 
-    for i, handle in ipairs(m.loaded_packages) do
-      -- Don't unload the network config package else crash when reloading
-      local should_not_unload = m.id == mod.ID and i == 2
-      if not should_not_unload then
-        Mod.release_resource_package(handle)
-      end
-    end
+		for i, handle in ipairs(m.loaded_packages) do
+			-- Don't unload the network config package else crash when reloading
+			local should_not_unload = m.id == mod.ID and i == 2
+			if not should_not_unload then
+				Mod.release_resource_package(handle)
+			end
+		end
 
-    m.state = "not_loaded"
-  else
-    self:print("error", "Mod index %i can't be unloaded, has not been loaded", index)
-  end
+		m.state = "not_loaded"
+	else
+		self:print("error", "Mod index %i can't be unloaded, has not been loaded", index)
+	end
 end
 
 mod:echo(mod.MOD_NAME .. " | v" .. mod.VERSION)
